87e16b73981281e18344b8ffa021ac50c501f950
AloozChips <aranyasahaa3@gmail.com>
2026-03-08 16:29:07 +0600

    include miuicam, viperfx; fix issues; cherry-pick
    Signed-off-by: AloozChips <aranyasahaa3@gmail.com>

--- evox.sh
+++ evox.sh
@@ -1,6 +1,7 @@
 #!/bin/bash
 
 rm -rf .repo/local_manifests
+rm -rf frameworks/av
 rm -rf packages/apps/Updater
 
 repo init -u https://github.com/Evolution-X/manifest -b bq2 --git-lfs
@@ -9,18 +10,28 @@ repo init -u https://github.com/Evolution-X/manifest -b bq2 --git-lfs
 #safer sync
 /opt/crave/resync.sh || repo sync -c -j$(nproc --all) --force-sync --no-clone-bundle --no-tags
 
+pushd frameworks/av
+git fetch https://github.com/AloozChips/frameworks_av.git && git cherry-pick aa1fd74e803570e1f8b4814ddf04b8b20c2e13c5
+popd
+
+pushd packages/apps/Updater
+git fetch https://github.com/AloozChips/evo_updater.git && git cherry-pick a633145592f88ac8c36236b20eead2047b9dc540
+popd
+
 rm -rf device/xiaomi/fog
 rm -rf vendor/xiaomi/fog
 rm -rf device/xiaomi/fog-kernel
 rm -rf hardware/xiaomi
-rm -rf packages/apps/Updater
+rm -rf vendor/xiaomi/camera
+rm -rf packages/apps/ViPER4AndroidFX
 rm -rf out/target/product/fog
 
 git clone https://github.com/AloozChips/device_xiaomi_fog.git device/xiaomi/fog -b evoq2 --depth 1
 git clone https://github.com/AloozChips/vendor_xiaomi_fog.git vendor/xiaomi/fog -b baklava-and-beyond --depth 1
 git clone https://github.com/AloozChips/device_xiaomi_fog-kernel.git device/xiaomi/fog-kernel -b ksu-and-bpf --depth 1
 git clone https://github.com/LineageOS/android_hardware_xiaomi.git hardware/xiaomi -b lineage-23.2 --depth 1
-git clone https://github.com/AloozChips/evo_updater.git packages/apps/Updater -b fog --depth 1
+git clone https://gitlab.com/ThankYouMario/proprietary_vendor_xiaomi_camera.git vendor/xiaomi/camera -b vauxite-sm6225 --depth 1
+git clone https://github.com/Evolution-X-Devices/packages_apps_ViPER4AndroidFX.git packages/apps/ViPER4AndroidFX -b bka --depth 1
 
 export TZ=Asia/Dhaka
 export BUILD_USERNAME=AloozChips
