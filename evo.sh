91ae92524124e72786e18675ddd5ca544aa9462a
AloozChips <aranyasahaa3@gmail.com>
2026-03-15 13:12:51 +0600

    dolbyevox

---
+++ evo.sh
@@ -0,0 +1,47 @@
+#!/bin/bash
+
+rm -rf .repo/local_manifests
+rm -rf frameworks/av
+rm -rf packages/apps/Updater
+
+repo init -u https://github.com/Evolution-X/manifest -b bq2 --git-lfs
+
+/opt/crave/resync.sh || repo sync -c -j$(nproc --all) --force-sync --no-clone-bundle --no-tags
+#safer sync
+/opt/crave/resync.sh || repo sync -c -j$(nproc --all) --force-sync --no-clone-bundle --no-tags
+
+pushd frameworks/av
+git fetch https://github.com/AloozChips/frameworks_av.git && git cherry-pick aa1fd74e803570e1f8b4814ddf04b8b20c2e13c5
+popd
+
+pushd packages/apps/Updater
+git fetch https://github.com/AloozChips/evo_updater.git && git cherry-pick a633145592f88ac8c36236b20eead2047b9dc540
+popd
+
+rm -rf device/xiaomi/fog
+rm -rf vendor/xiaomi/fog
+rm -rf device/xiaomi/fog-kernel
+rm -rf hardware/xiaomi
+rm -rf vendor/xiaomi/camera
+rm -rf packages/apps/DolbyUI
+rm -rf hardware/dolby
+rm -rf out/target/product/fog
+
+git clone https://github.com/AloozChips/device_xiaomi_fog.git device/xiaomi/fog -b evo-dolby --depth 1
+git clone https://github.com/AloozChips/vendor_xiaomi_fog.git vendor/xiaomi/fog -b baklava-and-beyond --depth 1
+git clone https://github.com/AloozChips/device_xiaomi_fog-kernel.git device/xiaomi/fog-kernel -b ksu-and-bpf --depth 1
+git clone https://github.com/LineageOS/android_hardware_xiaomi.git hardware/xiaomi -b lineage-23.2 --depth 1
+git clone https://gitlab.com/ThankYouMario/proprietary_vendor_xiaomi_camera.git vendor/xiaomi/camera -b vauxite-sm6225 --depth 1
+git clone https://github.com/swiitch-OFF-Lab/packages_apps_DolbyUI.git packages/apps/DolbyUI -b 16.0 --depth 1
+git clone https://github.com/swiitch-OFF-Lab/hardware_dolby.git hardware/dolby -b sony-1.5 --depth 1
+
+export TZ=Asia/Dhaka
+export BUILD_USERNAME=AloozChips
+export BUILD_HOSTNAME=crave
+export WITH_GMS=true
+export TARGET_BUILD_VARIANT=user
+
+. build/envsetup.sh
+lunch lineage_fog-bp4a-user
+m installclean
+m evolution -j$(nproc --all)
