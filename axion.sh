6dd86cce4c686b314b53a29ca98ce145f8dde5e8
𝙰𝚛𝚊𝚗𝚢𝚊🧃 <aranyasahaa3@gmail.com>
2026-03-19 10:26:17 +0600

    Update axion.sh

--- axion.sh
+++ axion.sh
@@ -3,7 +3,7 @@
 rm -rf .repo/local_manifests
 rm -rf packages/apps/Updater
 
-repo init -u https://github.com/AxionAOSP/android.git -b lineage-23.2 --git-lfs
+repo init -u https://github.com/AxionAOSP/android.git -b lineage-23.1 --git-lfs
 
 /opt/crave/resync.sh || repo sync -c -j$(nproc --all) --force-sync --no-clone-bundle --no-tags
 #safer sync
@@ -23,7 +23,7 @@ rm -rf out/target/product/fog
 git clone https://github.com/AloozChips/device_xiaomi_fog.git device/xiaomi/fog -b axion --depth 1
 git clone https://github.com/AloozChips/vendor_xiaomi_fog.git vendor/xiaomi/fog -b baklava-and-beyond --depth 1
 git clone https://github.com/AloozChips/device_xiaomi_fog-kernel.git device/xiaomi/fog-kernel -b ksu-and-bpf --depth 1
-git clone https://github.com/LineageOS/android_hardware_xiaomi.git hardware/xiaomi -b lineage-23.2 --depth 1
+git clone https://github.com/LineageOS/android_hardware_xiaomi.git hardware/xiaomi -b lineage-23.1 --depth 1
 git clone https://gitlab.com/ThankYouMario/proprietary_vendor_xiaomi_camera.git vendor/xiaomi/camera -b vauxite-sm6225 --depth 1
 
 export TZ=Asia/Dhaka
