a7c62d9b69122a5d5a3dbe8c8911242eb398c0b9
𝙰𝚛𝚊𝚗𝚢𝚊🧃 <aranyasahaa3@gmail.com>
2026-03-19 10:55:57 +0600

    Remove DolbyUI and hardware/dolby; clone new repos
    Added commands to remove DolbyUI and hardware/dolby directories, and clone their respective repositories.

--- axion.sh
+++ axion.sh
@@ -18,6 +18,8 @@ rm -rf vendor/xiaomi/fog
 rm -rf device/xiaomi/fog-kernel
 rm -rf hardware/xiaomi
 rm -rf vendor/xiaomi/camera
+rm -rf packages/apps/DolbyUI
+rm -rf hardware/dolby
 rm -rf out/target/product/fog
 
 git clone https://github.com/AloozChips/device_xiaomi_fog.git device/xiaomi/fog -b axion --depth 1
@@ -25,6 +27,8 @@ git clone https://github.com/AloozChips/vendor_xiaomi_fog.git vendor/xiaomi/fog 
 git clone https://github.com/AloozChips/device_xiaomi_fog-kernel.git device/xiaomi/fog-kernel -b ksu-and-bpf --depth 1
 git clone https://github.com/LineageOS/android_hardware_xiaomi.git hardware/xiaomi -b lineage-23.1 --depth 1
 git clone https://gitlab.com/ThankYouMario/proprietary_vendor_xiaomi_camera.git vendor/xiaomi/camera -b vauxite-sm6225 --depth 1
+git clone https://github.com/swiitch-OFF-Lab/packages_apps_DolbyUI.git packages/apps/DolbyUI -b 16.0 --depth 1
+git clone https://github.com/swiitch-OFF-Lab/hardware_dolby.git hardware/dolby -b sony-1.5 --depth 1
 
 export TZ=Asia/Dhaka
 export BUILD_USERNAME=AloozChips
