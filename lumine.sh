7c3edfa08e8986ad0c7694170b7a6b54df2c45c9
𝙰𝚛𝚊𝚗𝚢𝚊🧃 <aranyasahaa3@gmail.com>
2026-03-21 01:26:51 +0600

    Update lumine.sh

--- lumine.sh
+++ lumine.sh
@@ -2,6 +2,7 @@
 
 rm -rf .repo/local_manifests
 rm -rf packages/apps/Updater
+rm -rf vendor/lumine
 
 repo init -u https://github.com/LumineDroid/platform_manifest -b bellflower --git-lfs
 
@@ -21,12 +22,14 @@ rm -rf device/xiaomi/fog
 rm -rf vendor/xiaomi/fog
 rm -rf device/xiaomi/fog-kernel
 rm -rf hardware/xiaomi
+rm -rf vendor/xiaomi/camera
 rm -rf out/target/product/fog
 
 git clone https://github.com/AloozChips/device_xiaomi_fog.git device/xiaomi/fog -b lumine --depth 1
 git clone https://github.com/AloozChips/vendor_xiaomi_fog.git vendor/xiaomi/fog -b 25qx-staging --depth 1
 git clone https://github.com/AloozChips/device_xiaomi_fog-kernel.git device/xiaomi/fog-kernel -b ksu-and-bpf --depth 1
 git clone https://github.com/LineageOS/android_hardware_xiaomi.git hardware/xiaomi -b lineage-23.2 --depth 1
+git clone https://gitlab.com/ThankYouMario/proprietary_vendor_xiaomi_camera.git vendor/xiaomi/camera -b vauxite-sm6225 --depth 1
 
 export TZ=Asia/Dhaka
 export BUILD_USERNAME=AloozChips
