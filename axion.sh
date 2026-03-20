cc293ec0a90ba6bf2851545b877f4e5a9790210b
𝙰𝚛𝚊𝚗𝚢𝚊🧃 <aranyasahaa3@gmail.com>
2026-03-20 21:35:20 +0600

    Update axion.sh

--- axion.sh
+++ axion.sh
@@ -1,9 +1,8 @@
 #!/bin/bash
 
 rm -rf .repo/local_manifests
-rm -rf packages/apps/Updater
 
-repo init -u https://github.com/AxionAOSP/android.git -b lineage-23.1 --git-lfs
+repo init -u https://github.com/AxionAOSP/android.git -b lineage-23.2 --git-lfs
 
 /opt/crave/resync.sh || repo sync -c -j$(nproc --all) --force-sync --no-clone-bundle --no-tags
 #safer sync
@@ -18,10 +17,10 @@ rm -rf packages/apps/DolbyUI
 rm -rf hardware/dolby
 rm -rf out/target/product/fog
 
-git clone https://github.com/AloozChips/device_xiaomi_fog.git device/xiaomi/fog -b axion --depth 1
-git clone https://github.com/AloozChips/vendor_xiaomi_fog.git vendor/xiaomi/fog -b 25qx-staging --depth 1
+git clone https://github.com/AloozChips/device_xiaomi_fog.git device/xiaomi/fog -b axionbp4a --depth 1
+git clone https://github.com/AloozChips/vendor_xiaomi_fog.git vendor/xiaomi/fog -b baklava-and-beyond --depth 1
 git clone https://github.com/AloozChips/device_xiaomi_fog-kernel.git device/xiaomi/fog-kernel -b ksu-and-bpf --depth 1
-git clone https://github.com/LineageOS/android_hardware_xiaomi.git hardware/xiaomi -b lineage-23.1 --depth 1
+git clone https://github.com/LineageOS/android_hardware_xiaomi.git hardware/xiaomi -b lineage-23.2 --depth 1
 git clone https://gitlab.com/ThankYouMario/proprietary_vendor_xiaomi_camera.git vendor/xiaomi/camera -b vauxite-sm6225 --depth 1
 git clone https://github.com/swiitch-OFF-Lab/packages_apps_DolbyUI.git packages/apps/DolbyUI -b 16.0 --depth 1
 git clone https://github.com/swiitch-OFF-Lab/hardware_dolby.git hardware/dolby -b sony-1.5 --depth 1
@@ -38,13 +37,6 @@ export TARGET_PREBUILT_GOOGLE_CAMERA=false
 export TARGET_SHIP_PIXEL_LAUNCHER=false
 
 . build/envsetup.sh
-
-if axion fog user gms; then
-    echo ">>> Full GMS build selected"
-else
-    echo ">>> Full GMS failed, fallback to Pico"
-    axion fog user gms pico
-fi
-
+axion fog user gms
 axionSync
 ax -br user -j$(nproc --all)
