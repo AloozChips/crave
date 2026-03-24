27b3d0231295986f34f3eeb20fa04f15d5b2c366
𝙰𝚛𝚊𝚗𝚢𝚊🧃 <aranyasahaa3@gmail.com>
2026-03-24 18:09:22 +0600

    Update yaap.sh

--- yaap.sh
+++ yaap.sh
@@ -12,12 +12,20 @@ rm -rf device/xiaomi/fog
 rm -rf vendor/xiaomi/fog
 rm -rf device/xiaomi/fog-kernel
 rm -rf hardware/xiaomi
+rm -rf vendor/xiaomi/camera
+rm -rf packages/apps/ViPER4AndroidFX
+rm -rf packages/apps/DolbyUI
+rm -rf hardware/dolby
 rm -rf out/target/product/fog
 
-git clone https://github.com/AloozChips/device_xiaomi_fog.git device/xiaomi/fog -b yaapbq2 --depth 1
-git clone https://github.com/AloozChips/vendor_xiaomi_fog.git vendor/xiaomi/fog -b 25qx-staging --depth 1
+git clone https://github.com/AloozChips/device_xiaomi_fog.git device/xiaomi/fog -b yaapbp4a --depth 1
+git clone https://github.com/AloozChips/vendor_xiaomi_fog.git vendor/xiaomi/fog -b baklava-and-beyond --depth 1
 git clone https://github.com/AloozChips/device_xiaomi_fog-kernel.git device/xiaomi/fog-kernel -b ksu-and-bpf --depth 1
 git clone https://github.com/yaap/hardware_xiaomi.git hardware/xiaomi -b sixteen --depth 1
+git clone https://gitlab.com/ThankYouMario/proprietary_vendor_xiaomi_camera.git vendor/xiaomi/camera -b vauxite-sm6225 --depth 1
+git clone https://github.com/Evolution-X-Devices/packages_apps_ViPER4AndroidFX.git packages/apps/ViPER4AndroidFX -b bka --depth 1
+git clone https://github.com/swiitch-OFF-Lab/packages_apps_DolbyUI.git packages/apps/DolbyUI -b 16.0 --depth 1
+git clone https://github.com/swiitch-OFF-Lab/hardware_dolby.git hardware/dolby -b sony-1.5 --depth 1
 
 export TZ=Asia/Dhaka
 export BUILD_USERNAME=AloozChips
